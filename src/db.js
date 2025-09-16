7c43ef4b2dac9a5b2ab6407161a0f2e8911bd07f
Vitor Hugo Almeida <vitorhugo_as@hotmail.com>
2025-09-15 21:00:15 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -5,10 +5,11 @@ dotenv.config();
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: "",
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT || 3306),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
